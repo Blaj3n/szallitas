e23fb930b863b45510ca2dec131a8101a07d32fa
Blaj3n <benceulrich@gmail.com>
2024-08-20 05:20:39 +0200

    össztömeg meghatározása és kiiratása

--- szallitas.py
+++ szallitas.py
@@ -1,2 +1,8 @@
 tomegek = [16, 8, 9, 4, 3, 2, 4, 7, 7, 12, 3, 5, 4, 3, 2]
 
+print("2. feladat")
+
+osszesen = 0
+for tomeg in tomegek:
+    osszesen += tomeg
+print(f"A tárgyak tömegének összege: {osszesen} kg")
